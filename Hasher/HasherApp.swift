44c6140a17441bb175ded76e9484d5fe50b512ed
LemonPapperSeasoning <tish386@aucklanduni.ac.nz>
2022-09-05 00:35:40 +1200

    init commit

---
+++ Hasher/HasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  HasherApp.swift
+//  Hasher
+//
+//  Created by Birth of Venus on 5/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
